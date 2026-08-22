0969fd1cacf1d2427df941cfa8f7d7e96dd36215
Suraj Shrestha <surajshrestha@MacBook-Pro.local>
2026-08-22 12:19:52 -0500

    feat(r16): foundations - vector icon primitive and test setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,20 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+jest.mock('lucide-react-native', () => {
+  const React = require('react');
+  return new Proxy(
+    {},
+    {
+      get: (target, prop) => {
+        if (prop === '__esModule') return true;
+        const MockIcon = (props) => {
+          return React.createElement('IconMock', { ...props, 'data-icon-name': String(prop) });
+        };
+        MockIcon.displayName = String(prop);
+        return MockIcon;
+      },
+    }
+  );
+});
